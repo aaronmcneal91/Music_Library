6e5a56c86b2b4fe6dad0e46a630104a93ad62e8c
unknown <your email>
2022-03-11 11:58:02 -0500

    Created Serializer

--- music/models.py
+++ music/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Music(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    album = models.CharField(max_length=250)
+    release_date= models.DateField()
+    genre = models.CharField
+
